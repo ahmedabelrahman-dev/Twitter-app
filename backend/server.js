e6ae6fc6250c613df7f17121e8b84e65891d36b1
ahmedabelrahman-dev <rman79340@gmail.com>
2025-11-17 07:14:38 +0400

    feat: the backend

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import authRoutes from './routes/auth.routes.js';
+import dotenv from 'dotenv';
+import connectMongoDB from './db/connectMongoDB.js';
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use('/api/auth', authRoutes);
+
+app.listen(8000, () => {
+  console.log(`server is running on PORT ${PORT} `);
+  connectMongoDB();
+});
