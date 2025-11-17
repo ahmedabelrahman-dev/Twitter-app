9c6f3210bc708c65f781341981a1945501850735
ahmedabelrahman-dev <rman79340@gmail.com>
2025-11-17 21:48:53 +0400

    feat: added the user routes and controllers

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,4 @@
+export const getUserProfile = async (req, res) => {};
+export const updateUser = async (req, res) => {};
+export const followUnfollowUser = async (req, res) => {};
+export const getSuggestedUsers = async (req, res) => {};
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import authRoutes from './routes/auth.routes.js';
+import userRoutes from './routes/user.route.js';
 import dotenv from 'dotenv';
 import connectMongoDB from './db/connectMongoDB.js';
 import cookieParser from 'cookie-parser';
@@ -15,6 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 5000;
 
 app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
 
 app.listen(PORT, () => {
   console.log(`server is running on PORT ${PORT} `);
