59b26c59a20d2d1932bd57a3ee041f0722b7346c
ahmedabelrahman-dev <rman79340@gmail.com>
2025-11-17 22:25:47 +0400

    feat: added follow/unfollow user

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,12 +1,18 @@
-import express from "express";
-import { protectRoute } from "../middleware/protectRoute.js";
-import { followUnfollowUser, getSuggestedUsers, getUserProfile, updateUser } from "../controllers/user.controller.js";
+import express from 'express';
+import { protectRoute } from '../middleware/protectRoute.js';
+import {
+  followUnfollowUser,
+  getSuggestedUsers,
+  getUserProfile,
+  updateUser,
+} from '../controllers/user.controller.js';
 
 const router = express.Router();
 
-router.get("/profile/:username", protectRoute, getUserProfile);
-router.get("/suggested", protectRoute, getSuggestedUsers);
-router.post("/follow/:id", protectRoute, followUnfollowUser);
-router.post("/update", protectRoute, updateUser);
+router.get('/profile/:username', protectRoute, getUserProfile);
+router.post('/follow/:id', protectRoute, followUnfollowUser);
+router.get('/suggested', protectRoute, getSuggestedUsers);
+
+router.post('/update', protectRoute, updateUser);
 
 export default router;
