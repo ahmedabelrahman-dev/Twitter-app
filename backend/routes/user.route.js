a3f2df51cf6cad9f65b598f4390829d93fe81853
ahmedabelrahman-dev <rman79340@gmail.com>
2025-11-17 23:23:58 +0400

    feat: update user route and connected to cloudinary

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -12,7 +12,6 @@ const router = express.Router();
 router.get('/profile/:username', protectRoute, getUserProfile);
 router.post('/follow/:id', protectRoute, followUnfollowUser);
 router.get('/suggested', protectRoute, getSuggestedUsers);
-
 router.post('/update', protectRoute, updateUser);
 
 export default router;
