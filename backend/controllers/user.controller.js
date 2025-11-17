e8beb6c30fb5d96303e7b4da828bbf1a33579731
ahmedabelrahman-dev <rman79340@gmail.com>
2025-11-17 21:56:47 +0400

    feat: added get user pprofile

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,18 @@
-export const getUserProfile = async (req, res) => {};
+import User from '../models/user.model.js';
+
+export const getUserProfile = async (req, res) => {
+  const { username } = req.params;
+
+  try {
+    const user = await User.findOne({ username }).select('-password');
+    if (!user) return res.status(404).json({ message: 'User not found' });
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.log('Error in getUserProfile: ', error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
 export const updateUser = async (req, res) => {};
 export const followUnfollowUser = async (req, res) => {};
 export const getSuggestedUsers = async (req, res) => {};
