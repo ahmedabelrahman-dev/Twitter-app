6086605cc2db71935443b74637cad1a8ff5bb446
ahmedabelrahman-dev <rman79340@gmail.com>
2025-11-17 22:34:47 +0400

    feat: new notification model

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from '../models/user.model.js';
+import Notification from '../models/notification.model.js';
 
 export const getUserProfile = async (req, res) => {
   const { username } = req.params;
@@ -41,6 +42,13 @@ export const followUnfollowUser = async (req, res) => {
       await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
       await User.findByIdAndUpdate(req.user._id, { $push: { following: id } });
       // Send notification to the user
+      const newNotification = new Notification({
+        type: 'follow',
+        from: req.user._id,
+        to: userToModify._id,
+      });
+
+      await newNotification.save();
 
       res.status(200).json({ message: 'User followed successfully' });
     }
