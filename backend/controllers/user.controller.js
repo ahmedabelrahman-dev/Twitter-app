6547bacbac3e8573288ddd81f9d18e3606331ec5
ahmedabelrahman-dev <rman79340@gmail.com>
2025-11-18 22:21:09 +0400

    todo: update the user.controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -40,6 +40,7 @@ export const followUnfollowUser = async (req, res) => {
       await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
       await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
 
+      //todo :return the id of the user as a response
       res.status(200).json({ message: 'User unfollowed successfully' });
     } else {
       // Follow the user
@@ -53,7 +54,7 @@ export const followUnfollowUser = async (req, res) => {
       });
 
       await newNotification.save();
-
+      //todo :return the id of the user as a response
       res.status(200).json({ message: 'User followed successfully' });
     }
   } catch (error) {
