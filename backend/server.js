14c9e652b2fcc6e4c0259ebc69da64e67beb4ce4
ahmedabelrahman-dev <rman79340@gmail.com>
2025-11-18 21:57:32 +0400

    feat : added the notification routs

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import authRoutes from './routes/auth.routes.js';
 import userRoutes from './routes/user.route.js';
 import postRoutes from './routes/post.route.js';
+import notificationRoutes from './routes/notification.route.js';
 
 import dotenv from 'dotenv';
 import connectMongoDB from './db/connectMongoDB.js';
@@ -27,6 +28,7 @@ const PORT = process.env.PORT || 5000;
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
+app.use('/api/notifications', notificationRoutes);
 
 app.listen(PORT, () => {
   console.log(`server is running on PORT ${PORT} `);
