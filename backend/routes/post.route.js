e58580eb856d67f5770e9ddc22d9a0d7f20bde06
ahmedabelrahman-dev <rman79340@gmail.com>
2025-11-18 08:47:49 +0400

    feat: "get user posts" is added

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -7,10 +7,14 @@ import {
   likeUnlikePost,
   getAllPosts,
   getLikedPosts,
+  getFollowingPosts,
+  getUserPosts,
 } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
+router.get('/user/:username', protectRoute, getUserPosts);
+router.get('/following', protectRoute, getFollowingPosts);
 router.get('/likes/:id', protectRoute, getLikedPosts);
 router.get('/all', protectRoute, getAllPosts);
 router.post('/create', protectRoute, createPost);
