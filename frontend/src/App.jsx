25674844d68dd6107c8c7892dfd5f36c2f484cca
ahmedabelrahman-dev <rman79340@gmail.com>
2025-11-19 10:51:32 +0400

    feat: added profile page components and route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
+
 import HomePage from './pages/home/HomePage';
 import LoginPage from './pages/auth/login/LoginPage';
 import SignUpPage from './pages/auth/signup/SignUpPage';
+import NotificationPage from './pages/notification/NotificationPage';
+import ProfilePage from './pages/profile/ProfilePage';
+
 import Sidebar from './components/common/Sidebar';
 import RightPanel from './components/common/RightPanel';
-import NotificationPage from './pages/notification/NotificationPage';
+
 function App() {
   return (
     <div className="flex max-w-6xl mx-auto">
@@ -14,6 +18,7 @@ function App() {
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/notifications" element={<NotificationPage />} />
+        <Route path="/profile/:username" element={<ProfilePage />} />
       </Routes>
       <RightPanel />
     </div>
