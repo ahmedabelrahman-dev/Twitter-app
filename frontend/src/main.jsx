58aeea38b6b068be5ade049f3d2de4ea7a540a46
ahmedabelrahman-dev <rman79340@gmail.com>
2025-11-19 22:40:53 +0400

    feat: added react-query and implemented it in the frontend and installed its pakeges

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,8 @@ import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
