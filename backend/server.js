d205e0db52e1da7b0786c272d4a95f25fe417294
ahmedabelrahman-dev <rman79340@gmail.com>
2025-11-20 21:48:15 +0400

    feat: crete post with image

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,9 @@ cloudinary.config({
 const app = express();
 app.use(cookieParser());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '5mb' })); // to parse req.body
+// limit shouldn't be too high to prevent DOS
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 
 const PORT = process.env.PORT || 5000;
 
